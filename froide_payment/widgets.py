58fa8a0ec00f16835ba7caf880700ec6c675acb3
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2019-05-21 12:51:20 +0200

    Add own price input widget

---
+++ froide_payment/widgets.py
@@ -0,0 +1,14 @@
+from django import forms
+from django.conf import settings
+
+
+class PriceInput(forms.TextInput):
+    template_name = "donations/widgets/price_input.html"
+
+    def get_context(self, name, value, attrs):
+        ctx = super(PriceInput, self).get_context(name, value, attrs)
+        ctx['widget'].setdefault('attrs', {})
+        ctx['widget']['attrs']['class'] = 'form-control col-3'
+        ctx['widget']['attrs']['pattern'] = "[\\d\\.,]*"
+        ctx['currency'] = settings.DEFAULT_CURRENCY
+        return ctx
