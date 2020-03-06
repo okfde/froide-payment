a4b607056cf595cb13c30ce1be468a4737c9b92b
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2020-03-06 16:59:00 +0100

    Generate subscription orders 25 days in advance

--- froide_payment/utils.py
+++ froide_payment/utils.py
@@ -164,11 +164,11 @@ def create_recurring_order(subscription,
         return
 
     provider_name = subscription.plan.provider
-    seven_days_from_now = now + timedelta(days=7)
+    coming_month = now + timedelta(days=25)
 
     last_order = subscription.get_last_order()
 
-    if not force and last_order.service_end > seven_days_from_now:
+    if not force and last_order.service_end > coming_month:
         # Not yet due, set next_date correctly
         subscription.next_date = last_order.service_end
         subscription.save()
