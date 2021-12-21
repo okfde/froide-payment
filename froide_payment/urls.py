079652b291be4658580ad6d9fb651df93ae6d408
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2021-12-21 11:41:33 +0000

    Convert URLs config to use path

--- froide_payment/urls.py
+++ froide_payment/urls.py
@@ -1,23 +1,25 @@
-from django.conf.urls import url
+from django.urls import path
 
 from .views import (
-    start_payment, order_detail, order_success, subscription_detail,
-    subscription_cancel
+    start_payment,
+    order_detail,
+    order_success,
+    subscription_detail,
+    subscription_cancel,
 )
 
-app_name = 'froide_payment'
-
-TOKEN_PATTERN = '(?P<token>[^/]+)'
+app_name = "froide_payment"
 
 urlpatterns = [
-    url(r'^subscription/%s/$' % (TOKEN_PATTERN,),
-        subscription_detail, name='subscription-detail'),
-    url(r'^subscription/%s/cancel/$' % (TOKEN_PATTERN,),
-        subscription_cancel, name='subscription-cancel'),
-    url(r'^%s/$' % (TOKEN_PATTERN,),
-        order_detail, name='order-detail'),
-    url(r'^%s/success/$' % (TOKEN_PATTERN,),
-        order_success, name='order-success'),
-    url(r'^%s/payment/(?P<variant>[-\w]+)/$' % (TOKEN_PATTERN,),
-        start_payment, name='start-payment'),
+    path("subscription/<uuid:token>/", subscription_detail, name="subscription-detail"),
+    path(
+        "subscription/<uuid:token>/cancel/",
+        subscription_cancel,
+        name="subscription-cancel",
+    ),
+    path("<uuid:token>/", order_detail, name="order-detail"),
+    path("<uuid:token>/success/", order_success, name="order-success"),
+    path(
+        "<uuid:token>/payment/<slug:variant>/", start_payment, name="start-payment"
+    ),
 ]
